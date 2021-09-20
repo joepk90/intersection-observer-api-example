6e4e28c78125895568c9c9b42fd750181b6c043a
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2021-09-20 12:22:20 +0100

    add TODO

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import createObserver from './IntersectionObserver.js';
  * TODO:
  * - Setup intersection observer functionality for components with mulitple instances (use component ID?).
  * - Prevent re-initialisation of component if element has already been intersected.
+ * - create a generic component wrapper to handle component logic + script imports
  * - Setup the JS for the component + import lazy load the CSS as well.
  */
 
