7daa6a85e745e0cc0270c5ff74200793ab7b9bc7
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2021-09-20 11:45:58 +0100

    add TODOs

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
 import "../scss/style.scss";
 import createObserver from './IntersectionObserver.js';
 
+/**
+ * TODO:
+ * - Setup intersection observer functionality for components with mulitple instances (use component ID?).
+ * - Prevent re-initialisation of component if element has already been intersected.
+ * - Setup the JS for the component + import lazy load the CSS as well.
+ */
+
 document.addEventListener("DOMContentLoaded", async () => {
 
     const responsiveTabsElement = document.querySelector('.js-responsive-tabs');
